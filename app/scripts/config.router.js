e7570c8c754cadb49bbf14b69e3c87ab0c775299
Khoi Le <mr.vjcspy@gmail.com>
2016-04-10 15:22:20 +0700

    - init module

---
+++ app/scripts/config.router.js
@@ -0,0 +1,32 @@
+/**
+ * Created by vjcspy on 09/04/2016.
+ */
+app
+  .run(
+    ['$rootScope', '$state', '$stateParams',
+      function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+      }
+    ]
+  )
+  .config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider
+        .otherwise('/app/home');
+      $stateProvider
+        .state('app', {
+          abstract: true,
+          url: '/app',
+          templateUrl: 'views/app/menu.html',
+          controller: 'AppCtrl'
+        })
+        .state('app.home', {
+          url: '/home',
+          views: {
+            'menuContent': {
+              templateUrl: 'views/app/home.html'
+            }
+          }
+        })
+    }]);
---
+++ app/scripts/controllers/app/home.js
@@ -0,0 +1,5 @@
+/**
+ * Created by vjcspy on 10/04/2016.
+ */
+app.controller('HomeCtrl', ['$scope', '$log', function ($scope, $log) {
+}]);
